ced1d9245c5fe08d8ff53889b67994e497d0e8a6
NSMNIA <kevin.stoop@revolveddesign.com>
2022-09-20 09:05:26 +0200

    added custom props for notification
    Signed-off-by: NSMNIA <kevin.stoop@revolveddesign.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,17 @@ var _a;
 (_a = document.querySelector('button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
     Notification.requestPermission().then(perm => {
         if (perm === 'granted') {
-            new Notification('Hello world!');
+            const notification = new Notification('Hello world!', {
+                body: 'This is a notification',
+                icon: '/assets/images/icon.png',
+                data: {
+                    id: 1
+                },
+                // tag: 'hello-world'
+            });
+            notification.addEventListener("close", e => {
+                console.log("Notification closed", e);
+            });
         }
     });
 });
